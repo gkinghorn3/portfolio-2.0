c136f1b21634985f2bbc46c9c44438b21f079c7e
Grant <gkinghorn3@icloud.com>
2023-09-25 15:59:29 +0100

    header content adjusted

--- src/components/header/Header.Component.jsx
+++ src/components/header/Header.Component.jsx
@@ -1,21 +1,28 @@
-
-import './Header.styles.scss';
+import "./Header.styles.scss";
 
 const Header = () => {
-    return (
-        <div className='header-container'>  
-            <div className='text-content'>
-            <h1>Grant Kinghorn</h1>
-            <h2>Fullstack Web Developer</h2>
-
-
-            </div> 
-            <div className='headshot-container'>
-                <img src='/images/profile-pic.png' alt='Grant Kinghorn' className='header-image' />
-            </div>
-            
-        </div>
-    )
+  return (
+    <div className="header-container">
+      <div className="text-content">
+        <h1>Grant Kinghorn</h1>
+        <h2>Fullstack Web Developer</h2>
+        <p>
+          I am a passionate UK-based front-end developer with over four years of
+          experience working with a variety of libraries, frameworks & platforms such as
+          React, Redux, NextJS, NodeJs/ Express, SASS, BigCommerce, Shopify, Webflow and
+          WordPress. I build dynamic & intuitive user experiences that drive
+          sales increase exposure and deliver brand messages effectively.
+        </p>
+      </div>
+      <div className="headshot-container">
+        <img
+          src="/images/profile-pic.png"
+          alt="Grant Kinghorn"
+          className="header-image"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
