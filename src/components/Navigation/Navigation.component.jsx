d04ccc0426f5fe2cbfb9352fbb28ccbc1b0a2f58
Grant <gkinghorn3@icloud.com>
2023-10-11 16:54:15 +0100

    m

--- src/components/Navigation/Navigation.component.jsx
+++ src/components/Navigation/Navigation.component.jsx
@@ -1,24 +1,31 @@
-import { Link } from 'react-scroll';
+import { Link } from "react-scroll";
 
-import './Navigation.styles.scss';
+
+import "./Navigation.styles.scss";
 
 const Navigation = () => {
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <img src="/images/GKD..png" />
+      </div>
+      <div className="nav-links-container">
+        <Link to="about" smooth={true} duration={1000} className="nav-link">
+          Projects
+        </Link>
+        <Link to="about" smooth={true} duration={1000} className="nav-link">
+          Contact
+        </Link>
+        <a href="https://www.linkedin.com/in/grant-kinghorn-4b817a192/">
+          <img src="/images/icons/linkedIn.svg" />
+        </a>
+        <a href="https://github.com/gkinghorn3">
+          <img src="/images/icons/gitHub.svg" />
+        </a>
+      </div>
 
-    return (
-        <div className='nav-container'>
-        <div className='logo-container'>
-            <img src='/images/GKD..png' />
-        </div>
-        <div className='nav-links-container'> 
-            <Link to='about' smooth={true} duration={1000} className='nav-link'>Projects</Link>
-            <Link to='about' smooth={true} duration={1000} className='nav-link'>Contact</Link>
-            <a href='https://www.linkedin.com/in/grant-kinghorn-4b817a192/'><img src='/images/icons/linkedIn.svg' /></a>
-            <a href='https://github.com/gkinghorn3'><img src='/images/icons/gitHub.svg' /></a>
-        </div>
-    
-        </div>
-    )
-
-}
+    </div>
+  );
+};
 
 export default Navigation;
--- src/components/header/Header.Component.jsx
+++ src/components/header/Header.Component.jsx
@@ -6,28 +6,27 @@ const Header = () => {
   return (
     <div className="header-container">
       <div className="text-content">
-        
         <h2>Full Stack Web Developer</h2>
         <h1>GRANT KINGHORN</h1>
         <p>
-          I am a passionate UK-based front-end developer with over four years of
-          experience working with a variety of libraries, frameworks & platforms such as
-          React, Redux, NextJS, NodeJs/ Express, MongoDB, Firebase, SASS, BigCommerce, Shopify, Webflow and
-          WordPress. I build dynamic & intuitive user experiences that drive
-          sales increase exposure and deliver brand messages effectively.
+          Hi! I’m a web developer with expertise in designing and developing
+          exceptional digital experiences. My most used tech stack includes: JavaScript, React, Node/Express, MongoDB and Redux. Alongside
+          my preffered tools, I often work with WordPress, Webflow, Big Commerce, and Shopify.
+           My passion lies in creating user-friendly and visually
+          appealing digital experiences that cater to a wide range of clients.
+           If you have any questions or would like to discuss
+          your project further, please don’t hesitate to contact me.
         </p>
-        <BaseBtn className='header-cta' btnText="Contact" />
-        
+        <BaseBtn className="header-cta" btnText="Contact" />
       </div>
-      <div className='headshot-container'>
+      <div className="headshot-container">
         <img
           src="/images/profile-pic.png"
           alt="Grant Kinghorn"
-          className='header-image'
+          className="header-image"
         />
       </div>
-
-    </div>  
+    </div>
   );
 };
 
