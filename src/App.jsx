e5323c46cb114a1c5f49a6f767e082e57d1af087
Grant <gkinghorn3@icloud.com>
2023-10-12 11:05:24 +0100

    sidebar component added using react-burger-menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 
 import Navigation  from './components/Navigation/Navigation.component';
 import Header from './components/Header/Header.component';
+import Sidebar from './components/Sidebar/Sidebar';
 
-import './App.css'
+import './App.scss'
 
 function App() {
  
 
   return (
-    <>
-      <Navigation />
-      <Header />  
-    </>
+    <div id='outer-container'>
+      <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+      <div id='page-wrap'>
+        <Navigation />
+        <Header />  
+      </div>
+
+    </div>
   )
 }
 
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,23 @@
+import { bubble as Menu } from 'react-burger-menu';
+
+import './Sidebar.scss';
+
+export default props => {
+  return (
+    <Menu right>
+      <a className="menu-item" href="/">
+        Home
+      </a>
+      <a className="menu-item" href="/salads">
+        Salads
+      </a>
+      <a className="menu-item" href="/pizzas">
+        Pizzas
+      </a>
+      <a className="menu-item" href="/desserts">
+        Desserts
+      </a>
+    </Menu>
+  );
+};
+
