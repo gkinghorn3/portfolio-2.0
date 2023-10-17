f515947115c8de21588854cde3b69ee626dd4bbf
Grant <gkinghorn3@icloud.com>
2023-10-17 13:41:54 +0100

    m

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Fragment } from 'react';
 
 
 import Navigation  from './components/Navigation/Navigation.component';
-import Header from './components/Header/Header';
+import Header from './components/Header/header';
 import Sidebar from './components/Sidebar/Sidebar';
 import ProjectList from './components/ProjectList/ProjectList.component';
 import ContactForm from './components/ContactForm/ContactForm.component';
