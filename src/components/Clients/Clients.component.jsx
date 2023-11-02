2ff1539960ff710228e6f8c5bb0e34029bb638d0
grant kinghorn <gkinghorn3@icloud.com>
2023-11-02 22:06:48 +0000

    logo scroller

--- src/components/Clients/Clients.component.jsx
+++ src/components/Clients/Clients.component.jsx
@@ -1,41 +1,43 @@
-
-
-import "./clients.style.scss";
+import React, { useEffect, useRef } from 'react';
+import './clients.style.scss'; 
 
 const Clients = () => {
+  const scrollerRef = useRef(null);
 
-    return (
-      <section className='logos'>
-        {/* <h2>PAST CLIENT HIGHLIGHTS</h2> */}
-        <div className='logo-slide'>
-          <img src="images/clients-logos/centro.png" />
-          <img src="images/clients-logos/elios.png" />
-          <img src="images/clients-logos/FCN.png" />
-          <img src="images/clients-logos/lwp.png" />
-          <img src="images/clients-logos/mv.png" />
-          <img src="images/clients-logos/ndurance.png" />
-          <img src="images/clients-logos/toolden.png" />
-        </div>
-        <div className='logo-slide'>
-          <img src="images/clients-logos/centro.png" />
-          <img src="images/clients-logos/elios.png" />
-          <img src="images/clients-logos/FCN.png" />
-          <img src="images/clients-logos/lwp.png" />
-          <img src="images/clients-logos/mv.png" />
-          <img src="images/clients-logos/ndurance.png" />
-          <img src="images/clients-logos/toolden.png" />
-        </div>
+  useEffect(() => {
+    if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+      addAnimation();
+    }
 
+    function addAnimation() {
+      const scroller = scrollerRef.current;
+      scroller.setAttribute("data-animated", true);
 
+      const scrollerInner = scroller.querySelector(".scroller__inner");
+      const scrollerContent = Array.from(scrollerInner.children);
 
-      </section>
-    );
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        duplicatedItem.setAttribute("aria-hidden", true);
+        scrollerInner.appendChild(duplicatedItem);
+      });
+    }
+  }, []);
+
+  return (
+    <div className="scroller" data-speed="slow" ref={scrollerRef}>
+      <ul className="tag-list scroller__inner">
+        <li><img src='/images/clients-logos/centro.png'/></li>
+        <li><img src='/images/clients-logos/elios.png'/></li>
+        <li><img src='/images/clients-logos/FCN.png'/></li>
+        <li><img src='/images/clients-logos/lwp.png'/></li>
+        <li><img src='/images/clients-logos/mv.png'/></li>
+        <li><img src='/images/clients-logos/ndurance.png'/></li>
+        <li><img src='/images/clients-logos/toolden.png'/></li>
+
+      </ul>
+    </div>
+  );
 };
 
 export default Clients;
-
-
-
-
-
-
